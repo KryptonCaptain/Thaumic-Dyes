0af9c843107dac81d7583b6804a2488701cfabd7
KryptonCaptain <cjalcorn.s446@gmail.com>
2016-11-25 22:49:31 +1100

    Awkward.
    Forgot to update internal version number

--- src/main/java/thaumicdyes/common/ThaumicDyes.java
+++ src/main/java/thaumicdyes/common/ThaumicDyes.java
@@ -16,7 +16,7 @@ import thaumicdyes.common.items.ItemHandler;
 @Mod(
    modid = "thaumicdyes",
    name = "Thaumic Dyes",
-   version = "1.3",
+   version = "1.5.0",
    dependencies = "required-after:Thaumcraft@[4.2.3.5,)"
 )
 public class ThaumicDyes {
