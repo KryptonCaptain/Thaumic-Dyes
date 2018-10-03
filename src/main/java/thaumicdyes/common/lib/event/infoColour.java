b854b3a0c39a67ba51e502b495a7151e71b5bbf7
Fabboz <fabuz.135@gmail.com>
2018-10-03 08:08:22 +0200

    Add files via upload
    Icons Colored fix n.2

---
+++ src/main/java/thaumicdyes/common/lib/event/infoColour.java
@@ -0,0 +1,18 @@
+package thaumicdyes.common.lib.event;
+
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+
+public class infoColour {
+
+	public static void addColour(ItemStack stack, EntityPlayer player, List list, boolean par4) {
+		if (stack.getTagCompound() != null)
+			if (stack.getTagCompound().getCompoundTag("display") != null)
+				list.add(EnumChatFormatting.GRAY + "Colore: "
+						+ stack.getTagCompound().getCompoundTag("display").getTag("color").toString());
+	}
+
+}
